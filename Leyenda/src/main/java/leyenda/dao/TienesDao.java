58b443c94df1e1f44871263a1e7b6789e8de73d7
Marcopole <nicomarcopernic@gmail.com>
2019-04-22 23:46:02 +0200

    agregada función en DAO de Tienes

--- Leyenda/src/main/java/leyenda/dao/TienesDao.java
+++ Leyenda/src/main/java/leyenda/dao/TienesDao.java
@@ -1,12 +1,34 @@
 package leyenda.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 
+import leyenda.model.Caballero;
 import leyenda.model.Espada;
 import leyenda.model.Tiene;
 import leyenda.util.HibernateUtil;
 
 public class TienesDao {
+	
+	public List<Tiene> selectCaballero(String nombreCaballero){
+		List<Tiene> resultado = null;
+		try {
+			Session session = HibernateUtil.getSessionFactory().openSession();
+			Query q = session.createQuery("from Tiene c where :nombreCaballero = c.nombreCaballero");
+			q.setString("nombreCaballero", nombreCaballero);
+			resultado = q.list();
+			for(Tiene t: resultado) {
+				t.getEspada().getAtaqueEspada();
+			}
+			session.close();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return resultado;
+	}
+	
 	public void deleteTiene(Tiene tiene) {
 		try {
 			Session session = HibernateUtil.getSessionFactory().openSession();
